761b3c99fe58aafbd3871de0498c9e3e60e9310d
Szymon <szymon.zakrzewski@gmail.com>
2023-01-26 11:18:24 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -19,6 +19,19 @@
 
 IMAGE_SHAPE = (224, 224)
 
+def unzip_data(file_url):
+  """
+  Downloads data and unzips it
+  
+  Args:
+    * file_url (str): url link to file
+  """
+  !wget https://storage.googleapis.com/ztm_tf_course/food_vision/10_food_classes_10_percent.zip
+  # Unzip tour data
+  zip_ref = zipfile.ZipFile('10_food_classes_10_percent.zip', 'r')
+  zip_ref.extractall()
+  zip_ref.close()
+
 def create_tensorboard_callback(dir_name, experiment_name):
   """
   Creates TensorBoard callback
