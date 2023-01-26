3516693cf6e24c3309ac503f5c6bf626f9284974
Szymon <szymon.zakrzewski@gmail.com>
2023-01-26 14:12:32 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,34 +1,36 @@
-# import tensorflow as tf
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import zipfile
-# import os
-# import pathlib
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import zipfile
+import os
+import pathlib
+import datetime
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# import matplotlib.image as mpimg
+# Make the createing of our model a little easier
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation
+from tensorflow.keras import Sequential
+from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
 
-
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-# # Make the createing of our model a little easier
-# from tensorflow.keras.optimizers import Adam
-# from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation
-# from tensorflow.keras import Sequential
-# from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
+import tensorflow_hub as hub
+import zipfile
+import random
 
 IMAGE_SHAPE = (224, 224)
 
-def unzip_data(file_url):
+def unzip_data(file_url, file_name):
   """
   Downloads data and unzips it  
   Args:
     * file_url (str): url link to file
+    * file_name (str): name of the file to unzip
   """
-  import zipfile
   !wget file_url
   # Unzip tour data
-  zip_ref = zipfile.ZipFile('10_food_classes_10_percent.zip', 'r')
+  zip_ref = zipfile.ZipFile(file_name, 'r')
   zip_ref.extractall()
   zip_ref.close()
 
@@ -43,8 +45,6 @@ def create_tensorboard_callback(dir_name, experiment_name):
   Returns:
     * callback (tf.keras.callbacks.TensorBoard)
   """
-  import datetime
-  import tensorflow as tf
   log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime('%Y%m%d-%H%M%e')
   tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
   print(f"Saving logs to {log_dir}")
@@ -61,17 +61,10 @@ def view_random_image(target_dir, target_class):
   Returns:
     * The image data (numpy.array)     
   """
-  import random
-  import matplotlib.pyplot as plt
-  import os
-  import matplotlib.image as mpimg
-
   target_folder = target_dir+target_class
-
   # Get a random image path
   random_image = random.sample(os.listdir(target_folder),1)
   print(random_image)
-
   # Read in the image and plot it
   img = mpimg.imread(target_folder + "/" + random_image[0]) # IMPORTANT!!!!
   plt.imshow(img)
@@ -90,7 +83,6 @@ def plot_loss(history):
   Returns:
     * None
   """  
-  import matplotlib.pyplot as plt  
   loss = history.history['loss']
   val_loss = history.history['val_loss']
 
@@ -125,8 +117,6 @@ def load_and_prep_image(filename, img_shape=224):
   Returns:
     * image (Tensor): scaled (1/255.) tensor
   """
-
-  import tensorflow as tf
   # Read in the image
   img = tf.io.read_file(filename=filename)
   # Decode the read file into a tensor
@@ -144,7 +134,6 @@ def walk_trought_directory(dir_path):
   Args:
     * dir_path (str): directory path
   """
-  import os
   for dirpath, dirnames, filenames in os.walk(dir_path):
     print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'")
 
@@ -161,9 +150,6 @@ def create_model(model_url, num_classes=10):
     An uncompiled Keras Sequantial model with model_url as feature extractor 
     layer and Dense output layer with num_classes output neurons.
   """
-  import tensorflow_hub as hub
-  from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation
-  from tensorflow.keras import Sequential
   feature_extractor_layer = hub.KerasLayer(handle=model_url, 
                                            trainable=False, # freeze the already learned patterns
                                            name='feature_extractor_layer',
